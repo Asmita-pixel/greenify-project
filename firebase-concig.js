0807884ace3dd586b0a5def587d8b2def0a88c9d
Asmita-pixel <asmitabasak32@gmail.com>
2025-04-19 21:15:30 +0530

    Create firebase-concig.js

---
+++ firebase-concig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "A|zaSyCRdnt|Zp1unlDzOScXsmc613mqa9EcdZ8",
+  authDomain: "greenify-demo-2025.firebaseapp.com",
+  projectId: "greenify-demo-2025",
+  appId: "1:399490367487:web:13e469c9b4fa31d40671ca",
+};
+
+firebase.initializeApp(firebaseConfig);
